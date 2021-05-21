7f69dd1ef6ef2f400c04a8c8dc7e0e62cae0310b
Gaurav Gupta <30961298+28Gaurav@users.noreply.github.com>
2021-05-21 18:45:02 +0530

    fix #2

---
+++ app/src/main/java/com/example/stopwatch/splash_screen.java
@@ -0,0 +1,27 @@
+package com.example.stopwatch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+public class splash_screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(splash_screen.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 2000);
+
+    }
+}
